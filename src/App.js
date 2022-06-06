56cbec7d48d328cbd3a8db677cd86b459268950f
shubhxms <03.shubhamshah@gmail.com>
2022-06-06 11:34:13 +0530

    wallet connect func

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
-import { useEffect } from 'react/cjs/react.production.min';
-
+// import { useState, useEffect } from 'react/cjs/react.production.min';
+import {useState, useEffect} from 'react';
 // Constants
 const TWITTER_HANDLE = 'shubhxms';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
 
+  const [walletAdd, setWalletAdd] = useState(null);
+  
   const isWalletConnected = async () => {
     try{
       const {solana} = window;
       if(solana){
         if(solana.isPhantom){
           console.log("Phantom wallet found");
+          const response = await solana.connect({onlyIfTrusted:true});
+          console.log('connected with public key: ', response.publicKey.toString());
         }else{
           alert("Solana obj not found!")
         }
@@ -23,6 +27,23 @@ const App = () => {
     }
   }
 
+  const connectWallet = async () => {
+    const {solana} = window;
+    if(solana){
+      const response = await solana.connect();
+      console.log("Connected with public key: ", response.publicKey.toString());
+      setWalletAdd(response.publicKey.toString());
+    }
+  };
+
+  const renderWalletNotConnectedContainer = () => {
+    <button
+      className='cta=button connect-wallet-button'
+      onClick={connectWallet}
+      >
+        Connect to wallet
+      </button>
+  }
 
   useEffect(() => {
     const onLoad = async () => {
@@ -35,12 +56,13 @@ const App = () => {
 
   return (
     <div className="App">
-      <div className="container">
+      <div className={walletAdd ? 'authed-container' : 'container'}>
         <div className="header-container">
           <p className="header">🎭 syte - π</p>
           <p className="sub-text">
             crowd-sourced quote-wall ✨
           </p>
+          {!walletAdd && renderWalletNotConnectedContainer}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
