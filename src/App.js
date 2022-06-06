7cc8ce77086ad071081efc38f3c3e075beb50e95
shubhxms <03.shubhamshah@gmail.com>
2022-06-06 11:17:35 +0530

    wallet connect stuff

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
+import { useEffect } from 'react/cjs/react.production.min';
 
 // Constants
 const TWITTER_HANDLE = 'shubhxms';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+
+  const isWalletConnected = async () => {
+    try{
+      const {solana} = window;
+      if(solana){
+        if(solana.isPhantom){
+          console.log("Phantom wallet found");
+        }else{
+          alert("Solana obj not found!")
+        }
+      }
+    }catch(e){
+      console.log(e);
+    }
+  }
+
+
+  useEffect(() => {
+    const onLoad = async () => {
+      await isWalletConnected();
+    };
+    window.addEventListener('load', onLoad);
+    return () => window.removeEventListener('load', onLoad);
+  }, []);
+
+
   return (
     <div className="App">
       <div className="container">
