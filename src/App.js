74abb909d711ec4b9611fa42a02912e84c67141c
shubhxms <03.shubhamshah@gmail.com>
2022-06-17 17:01:51 +0530

    major changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
-// import { useState, useEffect } from 'react/cjs/react.production.min';
+import { Connection, PublicKey, clusterApiUrl } from '@solana/web3.js';
+import { Program, Provider, web3} from '@project-serum/anchor';
+import idl from './idl.json';
 import {useState, useEffect} from 'react';
+const { SystemProgram, Keypair } = web3;
+// import { useState, useEffect } from 'react/cjs/react.production.min';
+
 // Constants
+let baseAccount = Keypair.generate();
+const programId = new PublicKey(idl.metadata.address);
+const network = clusterApiUrl('devnet');
+const opts = {
+  preflightCommitment: "processed"
+}
+console.log("baseacconts", baseAccount);
 const TWITTER_HANDLE = 'shubhxms';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
@@ -10,7 +22,7 @@ const App = () => {
 
   const [walletAdd, setWalletAdd] = useState(null);
   const [inputVal, setInputVal] = useState('');
-  const [imgList, setImgList] = useState([]);
+  const [imgList, setImgList] = useState(null);
   const TEST_QUOTES = [
     'https://www.nitch.com/content/posts/1653856600-720w.jpg',
     'https://www.nitch.com/content/posts/1654027204-720w.jpg',
@@ -46,6 +58,14 @@ const App = () => {
     }
   };
 
+  const getProvider = () => {
+    const connection = new Connection(network, opts.preflightCommitment);
+    const provider = new Provider(
+      connection, window.solana, opts.preflightCommitment
+    );
+    return provider;
+  }
+
   const sendImg = async () => {
     if (inputVal.length > 0){
       console.log("link: ", inputVal);
@@ -65,7 +85,16 @@ const App = () => {
       </button>
   );
 
-  const renderWalletConnectedContainer = () => (
+  const renderWalletConnectedContainer = () => {
+    if (imgList === null){
+      return(
+        <div>
+          <button className='cta-button submit-gif-button' onClick={createImgAccount}>
+            One Time Initalization for Syte Account
+          </button>
+        </div>
+      )
+    }else{
     <div className='connected-container'>
       <form
         onSubmit={(evt) => {
@@ -77,14 +106,15 @@ const App = () => {
         <button type='submit' className='cta-button submit-gif-button'>Submit</button>
       </form>
       <div className='gif-grid'>
-        {imgList.map(qt => (
-          <div className='gif-item' key={qt}>
-            <img src={qt} alt={qt}/>
+        {imgList.map((qt, ind) => (
+          <div className='gif-item' key={ind}>
+            <img src={qt.imgLink} alt={qt}/>
           </div>
         ))}
       </div>
     </div>
-  );
+    }
+  };
 
   useEffect(() => {
     const onLoad = async () => {
@@ -94,13 +124,49 @@ const App = () => {
     return () => window.removeEventListener('load', onLoad);
   }, []);
 
+  const createImgAccount = async () => {
+    try{
+      const provider = getProvider();
+      const program = new Program(idl, programId, provider);
+      console.log("ping");
+      await program.rpc.startStuffOff({
+        account:{
+          baseAccount: baseAccount.publicKey,
+          user: provider.wallet.publicKey,
+          SystemProgram: SystemProgram.programId
+        },
+        signers: [baseAccount]
+      })
+      console.log("created a new baseaccount with address: ", baseAccount.publicKey.toString())
+      await getImgList()
+    }catch(e){
+      console.log(e);
+  }
+  }
+
+  const getImgList = async() => {
+    try{
+      const provider = getProvider();
+      const program = new Program(idl, programId, provider);
+      console.log(program.account.baseAccount)
+
+      const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+      console.log("got into", account);
+      setImgList(account.imgList)
+    }catch(e){
+      console.log("error in getImgList", e);
+      setImgList(null)
+    }
+  }
+
   useEffect(() => {
     if(walletAdd){
       console.log("fetching data");
+      getImgList()
     }
     // call solana program
     //set state
-    setImgList(TEST_QUOTES);
+    // setImgList(TEST_QUOTES);
   }, [walletAdd])
 
 
